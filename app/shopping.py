705a29a2d48d9b673fe95671dc9a9a58d12e1aaf
aredfern15 <77812013+aredfern15@users.noreply.github.com>
2021-04-04 23:34:04 -0400

    Refactored and test product lookup logic

--- app/shopping.py
+++ app/shopping.py
@@ -1,3 +1,5 @@
+#assisted by Professor Rossetti's screen sharing
+
 import os
 from datetime import datetime
 from pandas import read_csv
@@ -9,6 +11,18 @@ def format_usd(my_price):
     Examples: format_usd(10)
     """
     return f"${my_price:,.2f}"
+
+def lookup_product(product_id, all_products):
+     """
+     Params :
+         product_id (str) like "8"
+         all_products (list of dict) each dict should have "id", "name", "department", "aisle", and "price" attributes
+     """
+     matching_products = [p for p in all_products if str(p["id"]) == str(product_id)]
+     if any(matching_products):
+         return matching_products[0]
+     else:
+         return None
 
 # PREVENT ALL THE APP CODE FROM BEING IMPORTED
 # BUT STILL BE ABLE TO RUN IT FROM THE COMMAND LINE IS LIKE THIS...
@@ -29,9 +43,9 @@ if __name__ == "__main__":
         if selected_id.upper() == "DONE":
             break
         else:
-            matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-            if any(matching_products):
-                selected_products.append(matching_products[0])
+            matching_product = lookup_product(selected_id, products)
+            if matching_product:
+                 selected_products.append(matching_product)
             else:
                 print("OOPS, Couldn't find that product. Please try again.")
 
